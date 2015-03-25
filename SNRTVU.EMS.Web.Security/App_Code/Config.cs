b5949e8ea057e0372a0b78a15452a9c696cf7660
linkjr <linkjr@sina.com>
2015-03-25 23:08:24 +0800

    Show browser download url.

--- SNRTVU.EMS.Web.Security/App_Code/Config.cs
+++ SNRTVU.EMS.Web.Security/App_Code/Config.cs
@@ -8,7 +8,7 @@ namespace SNRTVU.EMS.Web.Security
 {
     public class Config
     {
-        private static string _siteName = ConfigurationManager.AppSettings["SiteName"];
+        private readonly static string _siteName = ConfigurationManager.AppSettings["SiteName"];
 
         /// <summary>
         /// 获取站点名称。
@@ -17,5 +17,27 @@ namespace SNRTVU.EMS.Web.Security
         {
             get { return _siteName; }
         }
+
+        #region 浏览器地址下载
+
+        private readonly static string _chromeDownload = ConfigurationManager.AppSettings["ChromeDownload"];
+        /// <summary>
+        /// 获取Chrome下载地址。
+        /// </summary>
+        public static string ChromeDownload
+        {
+            get { return _chromeDownload; }
+        }
+
+        private readonly static string _firefoxDownload = ConfigurationManager.AppSettings["FirefoxDownload"];
+        /// <summary>
+        /// 获取Firefox下载地址。
+        /// </summary>
+        public static string FirefoxDownload
+        {
+            get { return _firefoxDownload; }
+        }
+
+        #endregion
     }
 }
